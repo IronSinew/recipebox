4d3956491eacb14856df835f5a364fb14cc3622e
Gabriel Ceniza <gabstafari@yahoo.com>
2024-05-28 14:15:00 +0800

    let's go

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -255,6 +255,85 @@ const logout = () => {
       <main>
         <slot />
       </main>
+
+      <div
+        class="max-w-7xl mx-auto overflow-hidden mb-12 p-20 bg-[url('/assets/sub-footer-bg.png')] bg-center relative"
+      >
+        <div class="max-w-4xl relative mx-auto">
+          <div class="text-center mt-10">
+            <h1 class="text-4xl">
+              <span class="text-main-orange">Recreate</span> a Recipe!
+            </h1>
+            <p class="mt-4 text-lg">
+              Figure out what's in your food! Have you ever looked at a
+              commercial food product and wondered how to make it? Not a
+              problem! Use this professional technique to recreate a recipe from
+              any nutrition label
+            </p>
+            <Link :href="route('login')" class="mt-6 block">
+              <Button> LEARN MORE </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div
+          class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8"
+        >
+          <div class="flex justify-center items-center space-x-6 md:order-2">
+            <a
+              href="#"
+              class="text-gray-400 hover:text-gray-500"
+              target="_blank"
+              ><span class="sr-only">Facebook</span
+              ><svg
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                class="h-6 w-6"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                  clip-rule="evenodd"
+                ></path></svg></a
+            ><a
+              href="#"
+              class="text-gray-400 hover:text-gray-500"
+              target="_blank"
+              ><span class="sr-only">LinkedIn</span
+              ><svg
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                class="h-6 w-6"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                ></path></svg></a
+            ><a
+              href="#"
+              class="text-gray-400 hover:text-gray-500"
+              target="_blank"
+              ><span class="sr-only">X</span
+              ><svg
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                class="h-6 w-6"
+                aria-hidden="true"
+              >
+                <path
+                  d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z"
+                ></path></svg
+            ></a>
+          </div>
+          <p class="text-center text-sm leading-5">
+            Recipebox &copy; {{ new Date().getFullYear() }} All rights reserved.
+          </p>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
