ff86e288a977a9ad7cb75b88f1f91690e4658c86
Blake Severson <tarsonis21@gmail.com>
2025-07-17 13:50:45 -0700

    chore(deps): adding other eslint plugins

--- resources/js/Composables/theme-settings.js
+++ resources/js/Composables/theme-settings.js
@@ -1,7 +1,7 @@
 import { ref } from "vue";
 
 const isDarkMode = ref(localStorage.getItem("theme") === "dark");
-// eslint-disable-next-line require-jsdoc
+
 export function useThemeSettings() {
     const toggleDarkMode = () => (isDarkMode.value = !isDarkMode.value);
 
